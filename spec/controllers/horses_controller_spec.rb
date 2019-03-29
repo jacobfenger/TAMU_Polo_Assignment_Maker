067fed7af5a97e818df388d0de65054a6dc63dc1
EC2 Default User <ec2-user@ip-172-31-30-122.us-east-2.compute.internal>
2019-03-29 03:12:27 +0000

    rspec tests

--- spec/controllers/horses_controller_spec.rb
+++ spec/controllers/horses_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe HorsesController, type: :controller do
+    
+    describe "test_create" do
+        it 'should save valid horse' do
+            get :create, :horse => {:name => 'name',:age => '1'}
+            expect(response).to redirect_to(horses_path)
+        end
+        
+        it 'should display error for invalid horse' do
+            get :create, :horse => {:name => 'name'}
+            expect(flash[:error]).to eq("Both name and age are required.")
+            expect(response).to redirect_to(horses_path)
+        end
+    end
 
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -8,5 +8,29 @@ RSpec.describe SessionsController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+  
+  describe "test_create" do
+    it "should login with correct password" do
+      get :create,  :password => {:password => 'polo'}
+      expect(response).to redirect_to(root_path)
+      # expect to be logged in **
+    end
+    
+    it "should not login with incorrect password" do
+      get :create, :password => {:password => 'wrong'}
+      expect(flash[:danger]).to eq("Incorrect password entered.")
+      # redirect to somewhere **
+      # expect to not be logged in **
+    end
+    
+  end
+  
+  describe "test_destroy" do
+    it "should logout and go to home page" do
+      get :destroy
+      expect(response).to redirect_to(root_path)
+      # expect to not be logged in **
+    end
+  end
 
 end
