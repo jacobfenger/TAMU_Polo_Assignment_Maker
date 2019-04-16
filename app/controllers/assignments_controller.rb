b21e48fc77615c5dff46487fc68ecdf739cc0905
Jacob Fenger <jakefenger@gmail.com>
2019-04-16 18:33:32 -0500

    Updated sign up controller

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -8,6 +8,13 @@ class AssignmentsController < ApplicationController
 	def create 
 	  @assignment = Assignment.new
 	  @assignment.user = current_user
+	  p Horse.where(id: params["a"]["horse1"])
+	  @assignment.horse1_id = Horse.where(id: params["a"]["horse1"])
+	  @assignment.horse2_id = Horse.where(id: params["a"]["horse2"])
+	  @assignment.horse3_id = Horse.where(id: params["a"]["horse3"])
+	  @assignment.note = params["a"]["note"]
+
+	  p @assignment
 
 	  redirect_to practices_path
 	end
